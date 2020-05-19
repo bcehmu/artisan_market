002c82a3c3d94dc0c6f490ce02ae47c5ab1fb177
bcehmu <bcehmu@hotmail.com>
2020-05-19 16:01:33 +1000

    almost finishing mvp with new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Luthier.destroy_all
+Musician.destroy_all
 
+User.create(email: "bcehmu@hotmail.com", password: "123123")
+User.create(email: "hao.bcehmu@gmail.com", password: "123123")
+Luthier.create(user: User.where(email: "bcehmu@hotmail.com").first)
+Musician.create(user: User.where(email: "hao.bcehmu@gmail.com").first)
 
-# assuming there is a user (user_id: 2)(luthier) vl1@m.com and a user (user_id: 3)(musician) vm1@m.com
 Product.destroy_all
 Medium.destroy_all
 
-# Luthier.destroy_all
-# Luthier.create(user_id: 2, description: 'Virtual Luthier 1')
-# Musician.destroy_all
-# Musician.create(user_id: 3, description: 'Virtual Musician 1')
-
 for i in (1...20) do
-    Product.create(name: "Guitar Product #{i.to_s}", category: 'Guitar', description: 'Progressive', luthier_id: 4, base_price: 2500)
+    Product.create(name: "Guitar Product #{i.to_s}", category: 'Guitar', description: 'Progressive', luthier_id: Luthier.first.id, base_price: 2500)
 end
 
 for i in (1...20) do
-    Medium.create(name: "Medium #{i.to_s}", description: "Playthrough and Review #{i.to_s}", musician_id: 4, link: 'https://www.youtube.com/')
+    Medium.create(name: "Medium #{i.to_s}", description: "Playthrough and Review #{i.to_s}", musician_id: Musician.first.id, link: 'https://www.youtube.com/')
 end 
 
-# SubscribeMusician.create(user_id: 4, musician_id: 4)
-# SubscribeLuthier.create(user_id: 4, luthier_id: 4)
